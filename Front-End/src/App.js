78071f5e8a7bcb0c6e5a4903bfc5e2d42abb5b82
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-05-27 09:44:59 +0430

    create input task

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,19 +1,52 @@
 import React from "react";
-import { Nav, Navbar, Container } from "react-bootstrap";
+import {
+  Nav,
+  Navbar,
+  Container,
+  InputGroup,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 function App() {
   return (
-    <div className="text-bg-dark">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+    <div className="page-content">
+      <aside className="container-fluid">
+        <Navbar bg="dark" variant="dark" className="page-navbar">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+                alt="header icon"
+                src={require("./Image/logo.png")}
+                className="d-inline-block align-top"
+              />{" "}
+              ToDo List
+            </Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">History</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </aside>
+      <main className="container page-main">
+        <section className="page-imput-task">
+          <InputGroup>
+            <FormControl placeholder="Task description ..." />
+            <Form.Select className="page-imput-task-colour">
+              <option>Choose colour - default is random</option>
+              <option>yellow</option>
+              <option>orange</option>
+              <option>pink</option>
+              <option>purple</option>
+              <option>blue</option>
+            </Form.Select>
+            <Button variant="outline-warning">Button</Button>
+          </InputGroup>
+        </section>
+      </main>
     </div>
   );
 }
