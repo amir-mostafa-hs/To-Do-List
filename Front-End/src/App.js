35f7b5476db46fbf8f95987dfc5ea52af6022788
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-05-27 12:52:52 +0430

    create tack component

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Style/style.css";
 import {
   Nav,
   Navbar,
@@ -8,6 +9,34 @@ import {
   FormControl,
   Button,
 } from "react-bootstrap";
+import Task from "./components/Task";
+
+const tasksArray = [
+  {
+    title: "homework",
+    colorNumber: 2,
+    description:
+      "egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat",
+  },
+  {
+    title: "homework",
+    colorNumber: 3,
+    description:
+      "egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat",
+  },
+  {
+    title: "homework",
+    colorNumber: 4,
+    description:
+      "egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat",
+  },
+  {
+    title: "homework",
+    colorNumber: 5,
+    description:
+      "egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat",
+  },
+];
 
 function App() {
   return (
@@ -34,17 +63,30 @@ function App() {
       <main className="container page-main">
         <section className="page-imput-task">
           <InputGroup>
-            <FormControl placeholder="Task description ..." />
+            <FormControl placeholder="Task title ..." />
             <Form.Select className="page-imput-task-colour">
               <option>Choose colour - default is random</option>
-              <option>yellow</option>
-              <option>orange</option>
-              <option>pink</option>
-              <option>purple</option>
-              <option>blue</option>
+              <option>Yellow</option>
+              <option>Cyan</option>
+              <option>Light</option>
+              <option>Blue</option>
             </Form.Select>
             <Button variant="outline-warning">Button</Button>
           </InputGroup>
+          <InputGroup className="mt-2">
+            <InputGroup.Text>Description</InputGroup.Text>
+            <FormControl as="textarea" aria-label="With textarea" />
+          </InputGroup>
+        </section>
+        <section className="page-show-task">
+          {tasksArray?.map((task) => (
+            <Task
+              title={task.title}
+              description={task.description}
+              colorNumber={task.colorNumber}
+              key={Date.now() * Math.random()}
+            />
+          ))}
         </section>
       </main>
     </div>
