0cc46224ced71b76c30eb9ca5aacec9126dc8bc7
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-05-31 22:04:45 +0430

    add create task code

--- Front-End/src/components/CreateTask.js
+++ Front-End/src/components/CreateTask.js
@@ -1,21 +1,69 @@
-import { InputGroup, Form, FormControl, Button } from "react-bootstrap";
+import { useState } from "react";
+import { InputGroup, Form, FormControl, Button, Alert } from "react-bootstrap";
 const CreateTask = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [colour, setColour] = useState("Choose colour - default is random");
+  const [show, setShow] = useState(true);
+
+  const submitTask = () => {
+    if (!title || !description) {
+      setShow(true);
+      setTimeout(() => setShow(false), 5000);
+    } else {
+      setShow(false);
+    }
+  };
+
+  const EmptyAlert = () => {
+    if (show) {
+      return (
+        <aside className="notification">
+          <Alert
+            variant="danger"
+            onClose={() => setShow(false)}
+            dismissible
+            className="notification-alert"
+          >
+            <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+            <p>
+              The description or title field or both is empty. Please enter a
+              title and description
+            </p>
+          </Alert>
+        </aside>
+      );
+    }
+  };
   return (
     <aside>
+      <EmptyAlert />
       <InputGroup>
-        <FormControl placeholder="Task title ..." />
-        <Form.Select className="page-imput-task-colour">
+        <FormControl
+          placeholder="Task title ..."
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <Form.Select
+          className="page-imput-task-colour"
+          onChange={(e) => setColour(e.target.value)}
+        >
           <option>Choose colour - default is random</option>
           <option>Yellow</option>
           <option>Cyan</option>
           <option>Light</option>
           <option>Blue</option>
         </Form.Select>
-        <Button variant="outline-warning">Button</Button>
+        <Button variant="outline-warning" onClick={submitTask}>
+          Button
+        </Button>
       </InputGroup>
       <InputGroup className="mt-2">
         <InputGroup.Text>Description</InputGroup.Text>
-        <FormControl as="textarea" aria-label="With textarea" />
+        <FormControl
+          as="textarea"
+          aria-label="With textarea"
+          onChange={(e) => setDescription(e.target.value)}
+        />
       </InputGroup>
     </aside>
   );
